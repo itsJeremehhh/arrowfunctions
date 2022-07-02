133f5fdc8f695db0ac9c0a4c3b50f58ca50d619e
itsJeremehhh <107006437+itsJeremehhh@users.noreply.github.com>
2022-07-02 15:22:57 -0700

    Add files via upload

---
+++ arrowfunction.js
@@ -0,0 +1,34 @@
+//In this exercise, you’ll refactor some ES5 code into ES2015.
+
+
+function double (arr) {
+    return arr.map(function(val) {
+        return val *2;
+    });
+}
+
+//Refactor the above code to use two arrow functions. Turn it into a one-liner. 
+/* Write an ES2015 Version */
+
+function double (arr) {
+    return arr.map( val => {
+        return val * 2;
+    });
+}
+
+//the solution\\
+const double = arr => arr.map(val => val*2);
+
+//Replace ALL functions with arrow functions:
+
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+  const squareAndFindEvens = numbers => numbers.map( val => val ** 2).filter(square => square % 2 ===0)
